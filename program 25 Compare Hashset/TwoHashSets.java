3dfc0384f7e52143c85e100ffa5283f1899ab6b2
Amrutharavindran <116252422+Amrutharavindran@users.noreply.github.com>
2023-07-21 15:24:43 +0530

    Add files via upload

---
+++ program 25 Compare Hashset/TwoHashSets.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class TwoHashSets{
+public static void main(String[] args){
+HashSet<String>set1=new HashSet<String>();
+set1.add("a");
+set1.add("b");
+set1.add("c");
+set1.add("d");
+set1.add("e");
+Set<String>set2=new HashSet<String>();
+set2.add("a");
+set2.add("b");
+set2.add("c");
+set2.add("d");
+set2.add("e");
+System.out.println("Is Set 1 & Set 2 equal?:"+set1.equals(set2));
+}
+}
+
